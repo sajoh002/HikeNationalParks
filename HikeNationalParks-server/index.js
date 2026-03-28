c1b4bb93a8914d19a1c28661c34623c432fdc095
sajoh002 <sajoh002@gmail.com>
2026-03-28 11:42:10 -0400

    Add error handler

--- HikeNationalParks-server/index.js
+++ HikeNationalParks-server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
 
 const PORT = 8081;
 
@@ -16,6 +17,8 @@ app.use(function (req, res, next) {
   error.status = 404;
   next(error);
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, function () {
   console.log(`Server is starting on port ${PORT}`);
